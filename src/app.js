d9283b37e385a4e289c96e7a665511dfcab7bc1c
Andrés Bravo <andresbravomontes89@gmail.com>
2019-12-12 14:55:23 +0100

    getTeams

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ const typeDefs = `
        
     }
     type Query{
-        getTeams(_id: ID!): [Team]
+        getTeams: [Team]
     }
     type Mutation{
         addTeam(name: String!): Team!
@@ -85,7 +85,17 @@ const resolvers = {
     //     }
     // },
     Query:{
+       getTeams : async (parent, args, ctx, info) => {
+          const { client } = ctx;
+          
+          const db = client.db ("League"); 
+          const collection = db.collection ("Teams");
+
+          const result = await collection.find({}).toArray();
+
+          return result;
        
+       }
     },
     Mutation:{
 
